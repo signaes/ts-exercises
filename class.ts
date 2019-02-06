099b195961f38dfea1cc6f309cd46b35f1e71e57
Thiago Oliveira <thiago@signaes.com>
2019-02-06 18:19:04 -0200

    getter & setter

--- class.ts
+++ class.ts
@@ -2,13 +2,24 @@ interface Brand {
   name: string;
 }
 
-class Car {
-  protected age: number;
+class Vehicle {
+  wheels: number;
+  protected age: number = 0;
+}
+
+class Car extends Vehicle {
+  wheels = 4;
+
   constructor(public name: string, public brand: Brand) {
+    super();
   }
 
   toString() {
     return `${this.brand.name} ${this.name}`
+  }
+
+  incrementAge() {
+    this.age += 1;
   }
 }
 
@@ -19,3 +30,28 @@ const cherokee = new Car('Cherokee', jeep);
 
 console.log(cherokee)
 console.log(cherokee.toString())
+
+cherokee.incrementAge();
+console.log(cherokee)
+
+class Animal {
+  protected speaks: boolean = false;
+}
+
+class Human extends Animal {
+  private birthPlace: string;
+  speaks = true;
+
+  set origin(value: string) {
+    this.birthPlace = value;
+  }
+
+  get origin() {
+    return this.birthPlace;
+  }
+}
+
+const h = new Human()
+h.origin = 'Earth'
+
+console.log(h.origin)
